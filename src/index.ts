573cbab361d98716224440469d484e0d57dbad7a
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-21 13:42:56 +0100

    feat: add rate limiting middleware to protect API from abuse

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import authRouter from "./routes/authRouter.ts"
 import todoRouter from "./routes/todoRouter.ts"
 import { pool } from "./db/db.ts"
 import { errorHandler, routeNotFound } from "./middleware/errorHandler.ts"
+import { rateLimiter } from "./middleware/rateLimiter.ts"
 
 const port = process.env.TUDO_PORT || 3000
 const app = express()
+const limit = 60 * 60 * 1000;
+const maxTries = 30;
 
+app.use(rateLimiter(limit, maxTries))
 app.use(express.json())
 
 if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
